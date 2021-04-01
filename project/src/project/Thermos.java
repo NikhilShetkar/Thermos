bf6fc28d19e7155555cafd9df78c4cb0c63a3345
sysadmin <sysadmin@bcalab1-m03>
2021-04-01 09:43:01 +0530

    frist commit

---
+++ project/src/project/Thermos.java
@@ -0,0 +1,23 @@
+package project;
+
+public class Thermos {
+	String brand,liquid;
+	double qty;
+	
+	Thermos(String liquid, double qty,String brand)
+	{
+		this.liquid = liquid;
+	}
+	void add(double val)
+	{
+		qty = qty + val;
+	}
+	void display()
+	{
+		System.out.println("Liquid is"+liquid);
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
